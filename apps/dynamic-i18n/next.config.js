7f42cd1cccc93a951495cf19e20da3104a9c9e06
home <home@homes-MacBook-Pro.local>
2022-04-11 14:20:07 +0700

    fix(i18n): build wrong i18n

--- apps/dynamic-i18n/next.config.js
+++ apps/dynamic-i18n/next.config.js
@@ -1,6 +1,6 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx');
-const { i18n } = require('./next-i18next-server.config');
+const { i18n } = require('./next-i18next.config');
 
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
