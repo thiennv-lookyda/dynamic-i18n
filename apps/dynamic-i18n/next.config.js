fc69ab8a9617403898c1d326560dec67a97ceb4e
home <home@homes-MacBook-Pro.local>
2022-04-11 13:54:12 +0700

    fix(i18): revert

--- apps/dynamic-i18n/next.config.js
+++ apps/dynamic-i18n/next.config.js
@@ -1,8 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx');
-const getI18nConfig = require('./next-i18next.config');
-const i18n = getI18nConfig(true).i18n;
-console.log('🚀 ~ file: next.config.js ~ line 5 ~ i18n', i18n);
+const { i18n } = require('./next-i18next.config');
+
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
