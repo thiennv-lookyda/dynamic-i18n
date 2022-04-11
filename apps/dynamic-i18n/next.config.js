554996ba46341e272f38dfd50c0a6f1d5a1edf0a
home <home@homes-MacBook-Pro.local>
2022-04-11 13:49:14 +0700

    fix(i18n): logic for static prop

--- apps/dynamic-i18n/next.config.js
+++ apps/dynamic-i18n/next.config.js
@@ -1,12 +1,13 @@
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withNx = require('@nrwl/next/plugins/with-nx');
 const getI18nConfig = require('./next-i18next.config');
-
+const i18n = getI18nConfig(true).i18n;
+console.log('🚀 ~ file: next.config.js ~ line 5 ~ i18n', i18n);
 /**
  * @type {import('@nrwl/next/plugins/with-nx').WithNxOptions}
  **/
 const nextConfig = {
-  i18n: getI18nConfig(true).i18n,
+  i18n,
   nx: {
     // Set this to true if you would like to to use SVGR
     // See: https://github.com/gregberge/svgr
